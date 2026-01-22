90fdae28f2166c1f0d71004776eeff0e38fc41f1
Paolo Resi <61915667+Beastgeek23@users.noreply.github.com>
2026-01-22 16:48:33 +0000

    Feat: Created Scripts for every single minigame, each on is a child of interactable

---
+++ Assets/Scripts/Minigame Scripts/smallTalk.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class smallTalk : Interactable
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameName = "Drink Water";
+        gameObjective = new string[] { "Grab some water", "Listen to Coworkers Story", "Leave" };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

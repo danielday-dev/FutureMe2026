097442cccc67885a6dd2ce1e1853349ba4758946
Daniel Day <141579840+danielday-dev@users.noreply.github.com>
2026-01-23 10:43:49 +0000

    feat: upgrades panel
    need the game manager so committing and merging

--- Assets/Assets/UI/UpgradeButton.cs
+++ Assets/Assets/UI/UpgradeButton.cs
@@ -3,8 +3,13 @@ using UnityEngine.UI;
 
 public class UpgradeButton : MonoBehaviour
 {
+    enum UpgradeEffect
+    {
+        IncreaseCoffee,
+    }
+    
     [SerializeField] private Button prerequisiteButton;
-
+    [SerializeField] private UpgradeEffect effect;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,10 +17,37 @@ public class UpgradeButton : MonoBehaviour
         if (prerequisiteButton) { GetComponent<Button>().interactable = false; }
 
         GetComponent<Button>().onClick.AddListener(upgradeButtonPressed);
+        prerequisiteButton.onClick.AddListener(prerequisiteButtonPressed);
     }
 
     void upgradeButtonPressed()
     {
         Debug.Log("Upgrade Button Pressed");
+
+        //check if you have an upgrade point
+        if (true)
+        {
+            GetComponent<Button>().interactable = false;
+            activateUpgrade();
+        }
+    }
+
+    void activateUpgrade()
+    {
+        switch (effect)
+        {
+            case UpgradeEffect.IncreaseCoffee:
+                {
+                    
+                }
+                break;
+            default:
+                { Debug.Log("defaultID from button pressed, this shouldnt happen"); }break;
+        }
+    }
+
+    void prerequisiteButtonPressed()
+    {
+        GetComponent<Button>().interactable = true;
     }
 }

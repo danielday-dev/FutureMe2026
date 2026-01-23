f4ff673105fb8a25f9b8f94abcc4488359e64969
Paolo Resi <61915667+Beastgeek23@users.noreply.github.com>
2026-01-23 14:33:05 +0000

    WIP: Minigames are all hooked to objects and show text with the next objective

--- Assets/Interactables/interObj.cs
+++ Assets/Interactables/interObj.cs
@@ -1,23 +1,36 @@
 using UnityEngine;
+using TMPro;
+using System.Collections;
+
 
 public class interObj : Interactable
 {
     [SerializeField] string minigameName;
     minigameTemplate activity;
+    public TMP_Text textUI;
 
     public override void Execute(GameObject user)
     {
         minigame = GameObject.Find(minigameName);
         activity = minigame.gameObject.GetComponent<minigameTemplate>();
-        //Component[] test = minigame.gameObject.GetComponents(typeof(Component));
-        //for (int i = 0; i < test.Length; i++)
-        //{
-        //    Component comp = test[i];
-        //    print(comp.GetType());
-        //}
-        print(activity);
+        //print(activity);
         activity.nextObjective();
-        print(activity.currentObjective());
+        //print(activity.currentObjective());
+        displayText();
+    }
 
+    public void displayText()
+    {
+        RectTransform rect = textUI.rectTransform;
+        rect.anchoredPosition = new Vector2(0f, 200f);
+        textUI.gameObject.SetActive(true);
+        textUI.text = activity.currentObjective();
+        StartCoroutine(HideAfterDelay());
+    }
+
+    IEnumerator HideAfterDelay()
+    {
+        yield return new WaitForSeconds(3f);
+        textUI.gameObject.SetActive(false);
     }
 }
--- Assets/Interactables/pickUp.cs
+++ Assets/Interactables/pickUp.cs
@@ -1,10 +1,14 @@
+using TMPro;
 using UnityEngine;
+using System.Collections;
 
 public class pickUp : Interactable
 {
     [SerializeField] bool pickedUp = false;
     [SerializeField] string minigameName;
     minigameTemplate activity;
+    public TMP_Text textUI;
+
 
     public override void Execute(GameObject user)
     {
@@ -14,9 +18,24 @@ public class pickUp : Interactable
             minigame = GameObject.Find(minigameName);
             activity = minigame.gameObject.GetComponent<minigameTemplate>();
             activity.nextObjective();
-            print(activity.currentObjective());
+            displayText();
         }
         return;
+    }
+
+    public void displayText()
+    {
+        RectTransform rect = textUI.rectTransform;
+        rect.anchoredPosition = new Vector2(0f, 200f);
+        textUI.gameObject.SetActive(true);
+        textUI.text = activity.currentObjective();
+        StartCoroutine(HideAfterDelay());
+    }
+
+    IEnumerator HideAfterDelay()
+    {
+        yield return new WaitForSeconds(3f);
+        textUI.gameObject.SetActive(false);
     }
 
     //public string getName()
